80a1f2e4acbdb62f45524b48bb9c393823b02870
David Clarke <davidcl64@gmail.com>
2016-04-24 10:51:49 -0700

    Minor code cleanup

--- src/lib/consul.js
+++ src/lib/consul.js
@@ -13,7 +13,7 @@ const flattenKV   = (accum, record) => { accum[record.key] = record.value; retur
 const throwOnErr  = (err) => { if(err) { throw err; } };
 const obsFromArg0 = (arg) => rx.Observable.from(arg);
 
-const kvSet       = (consul) => (kv)   => consul.kv.set(kv, throwOnErr);
+const kvSet       = (consul) => (opts) => consul.kv.set(opts, throwOnErr);
 const kvGet       = (consul) => (opts) => rx.Observable.create((observer) => {
   consul.kv.get(opts, function(err, result) {
     if(err) { observer.onError(err); }
@@ -35,24 +35,22 @@ const init = (opts) => {
   const kvSetOpts = (path)  => (kv) => ({key: `${fullPath(prefix)(path)}${kv.key}`, value: JSON.stringify(kv.value)});
   
   return {
-    pull: (path) => {
-      return rx.Observable.from(_.flatten([path]))  // Ensure an array of paths
-        .map(fullPath(prefix))                      // get the full path (prefix/path)
-        .concatMap(pull(consul))                    // Grab values from consul - maintain order!
-        .concatMap(obsFromArg0)                     // Convert to observable sequences - maintain order!
-        .map(unPrefix)                              // Remove (prefix/path) from the keys
-        .map(parseValue)                            // Allows us to get real Javascript types (oxymoron?)
-        .map(consulToKV)                            // Pluck the key/value pairs
-        .reduce(flattenKV, {})                      // Reduce to a single object with keys and values
-        .map(util.unflatten);                       // Expand the object to its full hierarchy
-    },
+    pull: (path) => 
+      rx.Observable.from(_.flatten([path])) // Ensure an array of paths
+        .map(fullPath(prefix))              // Get the full path (prefix/path)
+        .concatMap(pull(consul))            // Grab values from consul - maintain order!
+        .concatMap(obsFromArg0)             // Convert to observable sequences - maintain order!
+        .map(unPrefix)                      // Remove (prefix/path) from the keys
+        .map(parseValue)                    // Allows us to get real Javascript types (oxymoron?)
+        .map(consulToKV)                    // Pluck the key/value pairs
+        .reduce(flattenKV, {})              // Reduce to a single object with keys and values
+        .map(util.unflatten),               // Expand the object to its full hierarchy
 
-    push: (path, data) => {
-      return rx.Observable.just(data)               // Start with a simple Object
-        .flatMap(util.kvMap)                        // Flatten to key/value pairs
-        .map(kvSetOpts(path))                       // Get consul opts => {key: prefix/path/key, value: val }
-        .do(kvSet(consul));                         // Fire off a set to consul 
-    }
+    push: (path, data) => 
+      rx.Observable.just(data)              // Start with a simple Object
+        .flatMap(util.kvMap)                // Flatten to key/value pairs
+        .map(kvSetOpts(path))               // Get consul opts => {key: prefix/path/key, value: val }
+        .do(kvSet(consul))                  // Fire off a set to consul 
   };
 };
 
