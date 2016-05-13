792d36ce87021c188be3781c8d803cc8d586ee5f
davidcl64 <davidcl64@gmail.com>
2016-05-13 15:03:20 -0700

    Keep logging to minimum when virtualbox/docker-machine not available (#6)

--- services/stop.sh
+++ services/stop.sh
@@ -6,13 +6,19 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 # Use that location as the working directory
 cd $dir
 
-# Check if the machine is currently stopped
-[ "$(docker-machine status pmpy)" == "Stopped" ] && exit 0
+# Check if both vboxmanage and docker-machine are installed - use them if they are
+if hash vboxmanage 2>/dev/null && hash docker-machine 2>/dev/null; then
+    # Check if the machine is currently stopped
+    [ "$(docker-machine status pmpy)" == "Stopped" ] && exit 0
 
-# Setup the docker environment so docker-compose works
-eval $(docker-machine env pmpy)
+    # Setup the docker environment so docker-compose works
+    eval $(docker-machine env pmpy)
 
-docker-compose stop && docker-compose rm -f
-docker-machine stop pmpy
+    docker-compose stop && docker-compose rm -f --all
+    docker-machine stop pmpy
+else
+    docker-compose stop && docker-compose rm -f --all
+fi
+
 
 echo "Playground is closed"
